574c201badf98e2d8773fa6066836262031088a9
aadorable <1615691183@qq.com>
2018-05-03 10:28:23 +0800

    seqqueue

---
+++ seqqueue/seqqueue.h
@@ -0,0 +1,25 @@
+#pragma once
+#include <stddef.h>
+
+#define SeqQueueMaxSize 1000
+
+typedef char SeqQueueType;
+
+typedef struct SeqQueue{
+    SeqQueueType data[SeqQueueMaxSize];
+    //[head, tail)
+    size_t head;
+    size_t tail;
+    //队列中元素个数
+    size_t size;
+}SeqQueue;
+
+void SeqQueueInit(SeqQueue* q);
+
+void SeqQueueDestory(SeqQueue* q);
+
+void SeqQueuePush(SeqQueue* q, SeqQueueType value);
+
+void SeqQueuePop(SeqQueue* q);
+
+int SeqQueueFront(SeqQueue* q, SeqQueueType* value);
